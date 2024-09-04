3a9d45627ea0d8c2dc3167850ffb49220ef28f99
Salino3 <flaviobcn012@gmail.com>
2024-09-04 14:55:09 +0200

    add dynamic header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
-document.getElementById("clickMe").addEventListener("click", function () {
-  alert("Button clicked!");
-});
+const clickMeButton = document.getElementById("clickMe");
+if (clickMeButton) {
+  clickMeButton.addEventListener("click", function () {
+    alert("Button clicked!");
+  });
+}
+
+function loadHeader() {
+  fetch("/src/components/header/header.component.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-container").innerHTML = data;
+
+      document.querySelectorAll("a").forEach((anchor) => {
+        document.getElementById("home-link").textContent = "Home";
+        document.getElementById("home-link").href = "/index.html";
+
+        document.getElementById("about-link").textContent = "About";
+        document.getElementById("about-link").href = "/src/html/about.html";
+
+        document.getElementById("contact-link").textContent = "Contact";
+        document.getElementById("contact-link").href = "/src/html/contact.html";
+      });
+    })
+    .catch((error) => console.error("Error cargando el encabezado:", error));
+}
+
+loadHeader();
